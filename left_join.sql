d38a89d208cc52db897cc55cdc2aadcace81fb6c
surya20003 <143775975+surya20003@users.noreply.github.com>
2025-06-24 14:51:43 +0530

    Delete left_join.sql

--- left_join.sql
+++
@@ -1,4 +0,0 @@
--- LEFT JOIN: Returns all records from the left table and the matched records from the right table
-SELECT employees.first_name, departments.department_name
-FROM employees
-LEFT JOIN departments ON employees.department_id = departments.department_id;
