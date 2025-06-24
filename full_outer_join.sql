2338d010c442550c3446960811b7752f4ec5cfe1
surya20003 <143775975+surya20003@users.noreply.github.com>
2025-06-24 14:50:34 +0530

    Delete full_outer_join.sql

--- full_outer_join.sql
+++
@@ -1,9 +0,0 @@
--- FULL OUTER JOIN: Returns all records when there is a match in either left or right table
--- Note: Not supported in MySQL directly. Use UNION of LEFT and RIGHT JOIN
-SELECT employees.first_name, departments.department_name
-FROM employees
-LEFT JOIN departments ON employees.department_id = departments.department_id
-UNION
-SELECT employees.first_name, departments.department_name
-FROM employees
-RIGHT JOIN departments ON employees.department_id = departments.department_id;
