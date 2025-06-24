9fc87b7c4bd20c13d30cb28462cb95ab54a83d48
surya20003 <143775975+surya20003@users.noreply.github.com>
2025-06-24 14:53:14 +0530

    Delete right_join.sql

--- right_join.sql
+++
@@ -1,4 +0,0 @@
--- RIGHT JOIN: Returns all records from the right table and the matched records from the left table
-SELECT employees.first_name, departments.department_name
-FROM employees
-RIGHT JOIN departments ON employees.department_id = departments.department_id;
