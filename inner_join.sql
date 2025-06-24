f9f22b22f6eed3978bafaf35f7d43d6d15ca2c05
surya20003 <143775975+surya20003@users.noreply.github.com>
2025-06-24 14:51:05 +0530

    Delete inner_join.sql

--- inner_join.sql
+++
@@ -1,4 +0,0 @@
--- INNER JOIN: Returns records that have matching values in both tables
-SELECT employees.first_name, departments.department_name
-FROM employees
-INNER JOIN departments ON employees.department_id = departments.department_id;
